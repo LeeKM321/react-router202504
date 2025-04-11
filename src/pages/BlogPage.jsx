ca62f0975ae98317ada2db5abd5fb700e3fca454
LeeKM321 <stephen4951@gmail.com>
2025-04-11 15:31:49 +0900

    #6. 블로그페이지 만들기

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import MainNav from '../components/MainNav';
+import PostCard from '../components/PostCard';
+import styles from './BlogPage.module.scss';
+import { posts } from '../assets/dummy-data';
 
 const BlogPage = () => {
   return (
-    <>
-      <h1>Blog Page 입니다.</h1>
-    </>
+    <div className={styles.blog}>
+      <div className={styles.grid}>
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+    </div>
   );
 };
 
